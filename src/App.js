076ad3931826c8e3e07e2038a1369a78d390f9a0
gyueunnim <rnrmfrbdms@gmail.com>
2023-07-12 11:35:50 +0900

    chore: set configuration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/messaging';
 
 function App() {
+  function requestPermission() {
+    console.log("권한 요청 중...");
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        console.log("알림 권한이 허용됨");
+  
+        // FCM 메세지 처리
+      } else {
+        console.log("알림 권한 허용 안됨");
+      }
+    });
+  }
+
+  useEffect(() => {
+    requestPermission()
+  })
+
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -4,6 +4,40 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from 'firebase/app'
+import { getMessaging, getToken, onMessage } from 'firebase/messaging'
+
+// Firebase 구성 정보 가져오기
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+// Firebase 앱 초기화
+const app = initializeApp(firebaseConfig);
+
+// Firebase Messaging 초기화
+const messaging = getMessaging(app);
+
+getToken(messaging, { vapidKey: 'BI-_sWLOnaahPCqAvw8zFydQz1yR24QkOp817njCh_VWqLCGjdSbr3MtvjvHqbrz8D1cBXKdB8rab5O3hDoTTmk' }).then((currentToken) => {
+  if(currentToken) {
+    console.log('FCM 토큰: ', currentToken);
+  } else {
+    console.log('FCM 토큰이 없습니다.');
+  }
+}).catch((error) => {
+  console.log('FCM 토큰 가져오기 오류 : ', error);
+})
+
+onMessage(messaging, (payload) => {
+  console.log('Message : ', payload)
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
